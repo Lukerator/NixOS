39e567dd68f438cc7475cbda40019ffd1b2accee
Lukerator <lucadarius41@yahoo.com>
2025-03-17 16:40:09 +0200

    update

--- system/foot.nix
+++ system/foot.nix
@@ -1,13 +1,13 @@
 {
 	programs.foot = {
-		enable = true;
+		enable = false;
 		# theme = "gruvbox-light";
 		theme = "gruvbox-dark";
 		settings = {
 			scrollback.lines = 0;
 			main = {
 				font = "JetBrainsMono Nerd Font:size=14";
-				pad = "3x3 center";
+				# pad = "3x3 center";
 			};
 		};
 	};
