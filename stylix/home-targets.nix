0e1eca0509a361d90a86e9990eadc83f2a89a614
Lukerator <lucadarius41@yahoo.com>
2025-03-21 22:23:51 +0200

    update

--- stylix/home-targets.nix
+++ stylix/home-targets.nix
@@ -2,13 +2,16 @@
 	config.stylix = {
 		image = ./catppuccin-macchiato.png;
 		targets = {
+			foot.enable = true;
+			zathura.enable = true;
+			hyprland = {
+				enable = true;
+				hyprpaper.enable = true;
+			};
 			firefox = {
 				colorTheme.enable = true;
 				profileNames = [ "Luke" ];
 			};
-			foot.enable = true;
-			hyprland.enable = true;
-			zathura.enable = true;
 		};
 	};
 }
