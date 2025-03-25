9092e8db7e3f4c420fb8abbca7f36d3bfa6db63f
Lukerator <lucadarius41@yahoo.com>
2025-03-25 17:14:57 +0200

    update

--- home/desktops/hyprland.nix
+++ home/desktops/hyprland.nix
@@ -33,8 +33,8 @@
         "nofocus,class:^$,title:^$,xwayland:1,floating:1,fullscreen:0,pinned:0"
       ];
       general = {
-        gaps_in = 5;
-        gaps_out = 10;
+        gaps_in = 6;
+        gaps_out = 12;
         border_size = 2;
         layout = "dwindle";
       };
