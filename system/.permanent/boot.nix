803fed5ba586d5592b22c58b191b94d3fae8e1d2
Lukerator <lucadarius41@yahoo.com>
2025-03-25 17:04:55 +0200

    update

--- system/.permanent/boot.nix
+++ system/.permanent/boot.nix
@@ -1,6 +1,9 @@
 {
   boot = {
+		consoleLogLevel = 0;
+		initrd.verbose = false;
     plymouth.enable = true;
+		kernelParams = [ "quiet" "udev.log_level=0" ];
     loader = {
       systemd-boot.enable = false;
       efi.canTouchEfiVariables = true;
