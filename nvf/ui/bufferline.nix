c245a7339cb1436cc5a93c5be0a2504f951ef534
Lukerator <lucadarius41@yahoo.com>
2025-03-26 21:30:43 +0200

    update

--- nvf/ui/bufferline.nix
+++ nvf/ui/bufferline.nix
@@ -1,18 +1,14 @@
 {
-  programs.nvf.settings = {
-    config = {
-      vim.tabline.bufferline = {
-        enable = true;
-        setupOpts.options = {
-          show_close_icon = false;
-          show_buffer_close_icons = false;
-        };
-        mappings = {
-          cycleNext = "<Tab>";
-          cyclePrevious = "<S-Tab>";
-          closeCurrent = "<leader>x";
-        };
-      };
+  programs.nvf.settings.config.vim.tabline.nvimBufferline = {
+    enable = true;
+    setupOpts.options = {
+      show_close_icon = false;
+      show_buffer_close_icons = false;
+    };
+    mappings = {
+      cycleNext = "<Tab>";
+      cyclePrevious = "<S-Tab>";
+      closeCurrent = "<leader>x";
     };
   };
 }
