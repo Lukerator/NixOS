c7b5611245132937801b094118e43084ce103d93
Lukerator <lucadarius41@yahoo.com>
2025-03-26 22:11:24 +0200

    update

--- nvf/keybinds/default.nix
+++ nvf/keybinds/default.nix
@@ -2,6 +2,7 @@
   imports = [
     ./leader.nix
     ./neotree.nix
+    ./codeactions.nix
     ./neovide.nix
   ];
 }
--- nvf/utilities/autocomplete.nix
+++ nvf/utilities/autocomplete.nix
@@ -1,10 +1,5 @@
-{pkgs, ...}: {
+{
   programs.nvf.settings.config.vim.autocomplete.blink-cmp = {
     enable = true;
-    sourcePlugins.codeium = {
-      enable = true;
-      package = pkgs.vimPlugins.codeium-nvim;
-      module = "Codeium";
-    };
   };
 }
