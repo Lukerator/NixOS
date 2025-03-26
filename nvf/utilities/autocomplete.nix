634af0522c509cd147736a7319fb6ef4b9bc2617
Lukerator <lucadarius41@yahoo.com>
2025-03-26 22:33:17 +0200

    update

--- nvf/utilities/autocomplete.nix
+++ nvf/utilities/autocomplete.nix
@@ -1,14 +1,6 @@
 {
   programs.nvf.settings.config.vim.autocomplete.blink-cmp = {
     enable = true;
-    setupOpts = {
-      keymap.preset = "super-tab";
-      sources.default = [
-        "lsp"
-        "path"
-        "buffer"
-        "snippets"
-      ];
-    };
+    friendly-snippets.enable = true;
   };
 }
