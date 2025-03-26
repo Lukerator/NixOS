58a324116bb7f49f105f3ece35b243fe23c8c15a
Lukerator <lucadarius41@yahoo.com>
2025-03-26 21:47:40 +0200

    update

--- nvf/lsp/haskell.nix
+++ nvf/lsp/haskell.nix
@@ -1,7 +1,6 @@
-{ lib, pkgs, ... }:
 {
   programs.nvf.settings.config.vim.languages.haskell = {
     enable = true;
-    lsp.package = [ (lib.getExe pkgs.haskellPackages.haskell-language-server) "--debug" ];
+    lsp.enable = false;
   };
 }
