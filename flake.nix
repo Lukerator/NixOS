983c630f056e2e585e2fbe7b8eaf404471f054a9
Lukerator <lucadarius41@yahoo.com>
2025-03-26 22:44:04 +0200

    update

--- flake.nix
+++ flake.nix
@@ -2,55 +2,52 @@
   inputs = {
     stylix.url = "github:danth/stylix";
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    nvf = {
-      inputs.nixpkgs.follows = "nixpkgs";
-      url = "github:notashelf/nvf";
-    };
-    # nixvim = {
+    # nvf = {
     #   inputs.nixpkgs.follows = "nixpkgs";
-    #   url = "github:nix-community/nixvim";
+    #   url = "github:notashelf/nvf";
     # };
+    nixvim = {
+      inputs.nixpkgs.follows = "nixpkgs";
+      url = "github:nix-community/nixvim";
+    };
     home-manager = {
       inputs.nixpkgs.follows = "nixpkgs";
       url = "github:nix-community/home-manager";
     };
   };
-  outputs =
-    {
-      home-manager,
-      # nixvim,
-      nixpkgs,
-      stylix,
-      nvf,
-      ...
-    }@inputs:
-    let
-      system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
-    in
-    {
-      nixosConfigurations.Luke-PC = nixpkgs.lib.nixosSystem {
-        inherit system;
-        specialArgs = { inherit inputs; };
-        modules = [
-          ./system
-          ./stylix
-          stylix.nixosModules.stylix
-          ./stylix/system-targets.nix
-        ];
-      };
-      homeConfigurations.luke = home-manager.lib.homeManagerConfiguration {
-        inherit pkgs;
-        modules = [
-          ./nvf
-          ./home
-          ./stylix
-          # ./nixvim
-          ./stylix/home-targets.nix
-          nvf.homeManagerModules.default
-          stylix.homeManagerModules.stylix
-          # nixvim.homeManagerModules.nixvim
-        ];
-      };
+  outputs = {
+    home-manager,
+    nixpkgs,
+    nixvim,
+    stylix,
+    # nvf,
+    ...
+  } @ inputs: let
+    system = "x86_64-linux";
+    pkgs = nixpkgs.legacyPackages.${system};
+  in {
+    nixosConfigurations.Luke-PC = nixpkgs.lib.nixosSystem {
+      inherit system;
+      specialArgs = {inherit inputs;};
+      modules = [
+        ./system
+        ./stylix
+        stylix.nixosModules.stylix
+        ./stylix/system-targets.nix
+      ];
     };
+    homeConfigurations.luke = home-manager.lib.homeManagerConfiguration {
+      inherit pkgs;
+      modules = [
+        ./nvf
+        ./home
+        ./stylix
+        # ./nixvim
+        ./stylix/home-targets.nix
+        # nvf.homeManagerModules.default
+        stylix.homeManagerModules.stylix
+        nixvim.homeManagerModules.nixvim
+      ];
+    };
+  };
 }
