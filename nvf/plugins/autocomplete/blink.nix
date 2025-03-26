9ac6d43776a557167cc05859e44597e36199b513
Lukerator <lucadarius41@yahoo.com>
2025-03-26 18:31:25 +0200

    update

--- nvf/plugins/autocomplete/blink.nix
+++ nvf/plugins/autocomplete/blink.nix
@@ -1,6 +1,19 @@
 {
   programs.nvf.settings.vim.autocomplete.blink-cmp = {
     enable = true;
-    sourcePlugins.lsp.enable = true;
+    sourcePlugins = {
+      lsp = {
+        enable = true;
+        package = "cmp-nvim-lsp";
+      };
+      path = {
+        enable = true;
+        package = "cmp-path";
+      };
+      treesitter = {
+        enable = true;
+        package = "cmp-treesitter";
+      };
+    };
   };
 }
