2406a4f334fc67073fbca267c2b26dc0a350317c
Lukerator <lucadarius41@yahoo.com>
2025-03-26 14:41:51 +0200

    update

--- nvf/base.nix
+++ nvf/base.nix
@@ -8,7 +8,7 @@
         enable = true;
         name = "HighlightYank";
       };
-      autocmds.highlightyank = {
+      autocmd.highlightyank = {
         enable = true;
         group = "HighlightYank";
         event = [ "TextYankPost" ];
