fa2a2ee9de3fd39077948c4ef68c42e159b80c28
Lukerator <lucadarius41@yahoo.com>
2025-03-26 18:21:11 +0200

    update

--- nvf/plugins/autocomplete/blink.nix
+++ nvf/plugins/autocomplete/blink.nix
@@ -1,51 +1,14 @@
-{ lib, ... }:
 {
   programs.nvf.settings.vim = {
     autocomplete.blink-cmp = {
       enable = true;
       friendly-snippets.enable = true;
       setupOpts = {
+        keymap.preset = "super-tab";
         cmdline = {
           keymap.preset = "super-tab";
           sources = [
             "path"
-          ];
-        };
-        keymap = {
-          preset = "none";
-          "<Tab>" = [
-            (lib.generators.mkLuaInline # lua
-              ''
-                cmp.mapping(function(fallback)
-                local luasnip = require("luasnip")
-                  if cmp.visible() then
-                    cmp.select_next_item()
-                  elseif luasnip.locally_jumpable(1) then
-                    luasnip.jump(1)
-                  else
-                    fallback()
-                  end
-                end)
-              ''
-            )
-          ];
-          "<CR>" = [
-            (lib.generators.mkLuaInline # lua
-              ''
-                cmp.mapping(function(fallback)
-                local luasnip = require("luasnip")
-                  if cmp.visible() then
-                    if luasnip.expandable() then
-                      luasnip.expand()
-                    else
-                      cmp.confirm({ select = true, })
-                    end
-                  else
-                    fallback()
-                  end
-                end)
-              ''
-            )
           ];
         };
       };
--- nvf/plugins/autocomplete/default.nix
+++ nvf/plugins/autocomplete/default.nix
@@ -1,5 +1,5 @@
 {
   imports = [
-    ./autocomplete
+    ./blink.nix
   ];
 }
