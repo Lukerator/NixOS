eafbaa253624dcf6c8048b022bb46dc66a58fed2
Lukerator <lucadarius41@yahoo.com>
2025-03-27 20:01:00 +0200

    update

---
+++ nixvim/plugins/utilities/completion/cmp.nix
@@ -0,0 +1,19 @@
+{
+  programs.nixvim.plugins.cmp = {
+    enable = true;
+    settings = {
+      completion.completeopt = "menu,menuone,noinsert";
+      snippet.expand = "function(args) require('luasnip').lsp_expand(args.body) end";
+      window = {
+        completion.border = "rounded";
+        documentation.border = "rounded";
+      };
+      sources = [
+        { name = "path"; }
+        { name = "buffer"; }
+        { name = "luasnip" }
+        { name = "nvim_lsp"; }
+      ];
+    };
+  };
+}
