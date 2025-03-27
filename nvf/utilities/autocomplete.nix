bc6d4f80b74e271f3afe5434ac024e86888e14cd
Lukerator <lucadarius41@yahoo.com>
2025-03-27 19:04:13 +0200

    update

--- nvf/utilities/autocomplete.nix
+++ nvf/utilities/autocomplete.nix
@@ -1,11 +1,6 @@
-{pkgs, ...}: {
+{
   programs.nvf.settings.config.vim.autocomplete.blink-cmp = {
     enable = true;
     friendly-snippets.enable = true;
-    sourcePlugins.codeium = {
-      enable = true;
-      package = pkgs.vimPlugins.codeium-nvim;
-      module = "Codeium";
-    };
   };
 }
