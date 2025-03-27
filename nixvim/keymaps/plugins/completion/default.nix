8a0b86f6a54988b5e1e8b21d2b284a7cac0b0dff
Lukerator <lucadarius41@yahoo.com>
2025-03-27 20:15:17 +0200

    update

---
+++ nixvim/keymaps/plugins/completion/default.nix
@@ -0,0 +1,5 @@
+{
+  import = [
+    ./cmp.nix
+  ];
+}
---
+++ nixvim/plugins/utilities/completion/codeium.nix
@@ -0,0 +1,13 @@
+{ lib, pkgs, ... }:
+{
+  nixpkgs.config.allowUnfreePredicate = pkg: builtins.elem (lib.getName pkg) [ "codeium" ];
+  programs.nixvim.plugins.codeium-nvim = {
+    enable = true;
+    settings.tools = {
+      curl = lib.getExe pkgs.curl;
+      gzip = lib.getExe pkgs.gzip;
+      uname = lib.getExe' pkgs.coreutils "uname";
+      uuidgen = lib.getExe' pkgs.util-linux "uuidgen";
+    };
+  };
+}
