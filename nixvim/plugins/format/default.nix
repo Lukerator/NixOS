ad4bce3cb1ba0dc1742396dcae2b78eb1464a814
Lukerator <lucadarius41@yahoo.com>
2025-03-31 21:08:03 +0300

    update

--- nixvim/plugins/format/default.nix
+++ nixvim/plugins/format/default.nix
@@ -50,5 +50,18 @@
           '';
       };
     };
+  keymaps = [
+    {
+      mode = "n";
+      key = "<leader>f";
+      options.desc = "[F]ormat file";
+      action.__raw = # lua
+        ''
+          function()
+            require('conform').format { async = true, lsp_fallback = true }
+          end
+        '';
+    }
+  ];
   };
 }
