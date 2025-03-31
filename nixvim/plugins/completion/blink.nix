b36bbbbfc5577100eb7ffe871f6c08f9ff9d840c
Lukerator <lucadarius41@yahoo.com>
2025-03-31 17:39:56 +0300

    update

---
+++ nixvim/plugins/completion/blink.nix
@@ -0,0 +1,6 @@
+{
+  programs.nixvim.plugins.blink-cmp = {
+    enable = true;
+    settings.keymap.preset = "super-tab";
+  };
+}
--- nixvim/plugins/completion/default.nix
+++ nixvim/plugins/completion/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./blink.nix
     ./cmp.nix
     ./modules
   ];
