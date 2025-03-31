026ae0a0549c4c2aa69987fb3ca0fca7bcc3a003
Lukerator <lucadarius41@yahoo.com>
2025-03-31 21:39:18 +0300

    update

--- nixvim/plugins/extra/typr.nix
+++ nixvim/plugins/extra/typr.nix
@@ -3,7 +3,7 @@
   programs.nixvim.extraPlugins = [
     (pkgs.vimUtils.buildVimPlugin {
       name = "typr";
-      src = pkgs.fetchFromGithub {
+      src = pkgs.fetchFromGitHub {
         repo = "typr";
         rev = "db0f98f";
         owner = "nvzone";
