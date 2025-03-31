ca3817ac8a5fbf307f07be5640b8a4d22a4e0840
Lukerator <lucadarius41@yahoo.com>
2025-03-31 18:37:32 +0300

    update

--- nixvim/plugins/completion/blink.nix
+++ nixvim/plugins/completion/blink.nix
@@ -15,50 +15,7 @@
         "buffer"
         "snippets"
       ];
-      keymap = {
-        preset = "none";
-        "<CR>".__raw =
-          ''
-            cmp.mapping(function(fallback)
-            local luasnip = require("luasnip")
-              if cmp.visible() then
-                if luasnip.expandable() then
-                  luasnip.expand()
-                else
-                  cmp.confirm({ select = true, })
-                end
-              else
-                fallback()
-              end
-            end)
-          '';
-    "<Tab>".__raw =
-      ''
-        cmp.mapping(function(fallback)
-        local luasnip = require("luasnip")
-          if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(1) then
-            luasnip.jump(1)
-          else
-            fallback()
-          end
-        end)
-      '';
-    "<S-Tab>".__raw =
-      ''
-        cmp.mapping(function(fallback)
-        local luasnip = require("luasnip")
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.locally_jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end)
-      '';
-      };
+      keymap.preset = "super-tab";
     };
     };
   };
