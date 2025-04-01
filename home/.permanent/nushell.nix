b1664133af8542c121029a05b331ce81a1274d5f
Lukerator <lucadarius41@yahoo.com>
2025-04-01 15:49:11 +0300

    update

--- home/.permanent/nushell.nix
+++ home/.permanent/nushell.nix
@@ -1,32 +1,35 @@
 { pkgs, ... }:
 {
-  programs.nushell = {
-    enable = true;
-    settings = {
-      show_banner = false;
-      PROMPT_COMMAND_RIGHT = "";
-      completions.external = {
-        enable = true;
-        max_results = 20;
+  programs = {
+    oh-my-posh.enableNushellIntegration = true;
+    nushell = {
+      enable = true;
+      settings = {
+        show_banner = false;
+        prompt_command_right = { };
+        completions.external = {
+          enable = true;
+          max_results = 20;
+        };
       };
-    };
-    plugins = with pkgs.nushellPlugins; [
-      formats
-      gstat
-      highlight
-    ];
-    shellAliases = {
-      ls = "lsd";
-      eww = "eww -c ~/.config/nixos/eww";
-      update = "git-update ; update-no-git";
-      upgrade = "git-update ; upgrade-no-git ; git-update";
-      home-update = "git-update ; home-update-no-git";
-      system-update = "git-update ; system-update-no-git";
-      system-update-no-git = "nh os switch ; nh clean all";
-      home-update-no-git = "nh home switch -- --impure ; nh clean all";
-      update-no-git = "nh os switch ; nh home switch -- --impure ; nh clean all";
-      upgrade-no-git = "nh os switch -u ; nh home switch -- --impure ; nh clean all";
-      git-update = ''cd ~/.config/nixos ; git add --all ; git push ; git commit -a -m "update" ; cd -'';
+      plugins = with pkgs.nushellPlugins; [
+        formats
+        gstat
+        highlight
+      ];
+      shellAliases = {
+        ls = "lsd";
+        eww = "eww -c ~/.config/nixos/eww";
+        update = "git-update ;; update-no-git";
+        upgrade = "git-update ;; upgrade-no-git ;; git-update";
+        home-update = "git-update ;; home-update-no-git";
+        system-update = "git-update ;; system-update-no-git";
+        system-update-no-git = "nh os switch ;; nh clean all";
+        home-update-no-git = "nh home switch -- --impure ;; nh clean all";
+        update-no-git = "nh os switch ;; nh home switch -- --impure ;; nh clean all";
+        upgrade-no-git = "nh os switch -u ;; nh home switch -- --impure ;; nh clean all";
+        git-update = ''cd ~/.config/nixos ;; git add --all ;; git push ;; git commit -a -m "update" ;; cd -'';
+      };
     };
   };
 }
