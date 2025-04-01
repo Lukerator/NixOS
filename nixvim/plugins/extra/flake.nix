e13500590201d046562c388379b5d9568682109e
Lukerator <lucadarius41@yahoo.com>
2025-04-01 22:38:26 +0300

    update

--- nixvim/plugins/extra/flake.nix
+++ nixvim/plugins/extra/flake.nix
@@ -1,7 +1,13 @@
 { inputs, pkgs, ... }:
 {
   overlays = [ inputs.awesome-neovim-plugins.overlays.default ];
-  programs.nixvim.extraPlugins = with pkgs.awesomeNeovimPlugins; [
-    exercism-nvim
-  ];
+  programs.nixvim = {
+    extraPlugins = with pkgs.awesomeNeovimPlugins; [
+      exercism-nvim
+    ];
+    extraConfigLua = # lua
+      "
+        require('exercism-nvim').setup()
+      ";
+  };
 }
