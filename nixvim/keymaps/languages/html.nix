b915c46d31f13f4b2f37450e8d1cc8bd2854e7e9
Lukerator <lucadarius41@yahoo.com>
2025-04-01 14:08:51 +0300

    update

--- nixvim/keymaps/languages/html.nix
+++ nixvim/keymaps/languages/html.nix
@@ -2,8 +2,8 @@
   programs.nixvim.keymaps = [
     {
       mode = "n";
-      key = "<leader>gh";
-      action = ":! zen-twilight % &<CR><CR>";
+      key = "<leader>gw";
+      action = ":! firefox % &<CR><CR>";
       options = {
         silent = true;
         desc = "[G]o [H]tml file";
