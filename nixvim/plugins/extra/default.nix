b2f8ab6d47bc0b0cad844a89ac22e81b089db9cb
Lukerator <lucadarius41@yahoo.com>
2025-04-01 22:24:24 +0300

    update

--- nixvim/plugins/extra/default.nix
+++ nixvim/plugins/extra/default.nix
@@ -20,14 +20,14 @@
         sha256 = "";
       };
     })
-    (pkgs.vimUtils.buildVimPlugin {
-      name = "exercism";
-      src = pkgs.fetchFromGitHub {
-        rev = "e123320";
-        owner = "2KAbhishek";
-        repo = "exercism.nvim";
-        sha256 = "sha256-ysuDkCJ+SUap9q1AVmJHg984B3yHiyT2Jq0uTZ0wtuM=";
-      };
-    })
+    # (pkgs.vimUtils.buildVimPlugin {
+    #   name = "exercism";
+    #   src = pkgs.fetchFromGitHub {
+    #     rev = "e123320";
+    #     owner = "2KAbhishek";
+    #     repo = "exercism.nvim";
+    #     sha256 = "sha256-ysuDkCJ+SUap9q1AVmJHg984B3yHiyT2Jq0uTZ0wtuM=";
+    #   };
+    # })
   ];
 }
