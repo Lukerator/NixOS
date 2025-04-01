b0053e85d54f4469b9d370cb744b8d7c154cb481
Lukerator <lucadarius41@yahoo.com>
2025-04-01 19:00:30 +0300

    update

--- nixvim/plugins/misc/default.nix
+++ nixvim/plugins/misc/default.nix
@@ -2,6 +2,7 @@
   programs.nixvim.plugins = {
     mini.enable = true;
     noice.enable = true;
+    image.enable = true;
     notify.enable = true;
     comment.enable = true;
     twilight.enable = true;
