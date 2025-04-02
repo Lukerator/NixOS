c4c4b72942c0af0545e90f6e102b9423f34390b6
Lukerator <lucadarius41@yahoo.com>
2025-04-02 17:33:12 +0300

    update

--- nvf/default.nix
+++ nvf/default.nix
@@ -99,7 +99,7 @@
 
     settings.vim = {
 
-      global = {
+      globals = {
         # Set <space> as the leader key
         # See `:help mapleader`
         #  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
