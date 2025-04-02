6d62d37af7218482c95c5ca6c45dd53e7904eb0d
Lukerator <lucadarius41@yahoo.com>
2025-04-02 22:31:23 +0300

    update

--- nvf/default.nix
+++ nvf/default.nix
@@ -707,7 +707,7 @@ in
           notify_on_error = false;
           format_on_save = {
             timeout_ms = 500;
-            lsp_format = 'fallback';
+            lsp_format = "fallback";
           };
           formatters_by_ft = {
             lua = [ "stylua" ];
