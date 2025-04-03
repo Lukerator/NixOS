d264c2875aa3ab4e739d6174a10485f4b0746b7a
Lukerator <lucadarius41@yahoo.com>
2025-04-03 23:40:40 +0300

    update

--- nvf/keymaps/misc/default.nix
+++ nvf/keymaps/misc/default.nix
@@ -1,6 +1,6 @@
 {
   imports = [
-    ./errors.nix
+    # ./errors.nix
     ./movement.nix
     ./search.nix
     ./terminal.nix
--- nvf/plugins/files/default.nix
+++ nvf/plugins/files/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./neotree.nix
     ./telescope.nix
   ];
 }
