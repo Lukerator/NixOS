119d5879e216afe3465481092e22f3901a5418ea
Lukerator <lucadarius41@yahoo.com>
2025-04-03 23:23:18 +0300

    update

--- nvf/default.nix
+++ nvf/default.nix
@@ -1,5 +1,6 @@
 {
   imports = [
+    ./keymaps
     ./misc
     ./plugins
   ];
--- nvf/keymaps/plugins/files/telescope.nix
+++ nvf/keymaps/plugins/files/telescope.nix
@@ -8,17 +8,6 @@
     findFiles = "<leader>sf";
     gitStash = "<leader>svx";
     gitStatus = "<leader>svs";
-    treesitter = "<leader>ss";
-    diagnostics = "<leader>sld";
-    findProjects = "<leader>sp";
     gitBranches = "<leader>svb";
-    gitCommits = "<leader>svcw";
-    lspReferences = "<leader>slr";
-    lspDefinitions = "<leader>slD";
-    gitBufferCommits = "<leader>svcb";
-    lspImplementations = "<leader>sli";
-    lspTypeDefinitions = "<leader>slt";
-    lspDocumentSymbols = "<leader>slsb";
-    lspWorkspaceSymbols = "<leader>slsw";
   };
 }
