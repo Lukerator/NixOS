768db27b140cf2d5b4cafed0e59e31eb4bf2c913
Lukerator <lucadarius41@yahoo.com>
2025-04-04 10:55:48 +0300

    update

--- nvf/keymaps/plugins/autocomplete/cmp.nix
+++ nvf/keymaps/plugins/autocomplete/cmp.nix
@@ -1,11 +1,2 @@
 {
-  config.vim.autocomplete.nvim-cmp.mappings = {
-    close = "";
-    complete = "";
-    confirm = "";
-    next = "";
-    previous = "";
-    scrollDocsDown = "";
-    scrollDocsUp = "";
-  };
 }
