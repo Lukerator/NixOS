8f61a8aef4dea7dc2e7b920df64775b51cd0e49b
Lukerator <lucadarius41@yahoo.com>
2025-04-04 08:44:24 +0300

    update

---
+++ nvf/plugins/autocomplete/blink.nix
@@ -0,0 +1,34 @@
+{ pkgs, ... }:
+{
+  config.vim.autocomplete = {
+    enableSharedCmpSources = true;
+    blink-cmp = {
+      enable = true;
+      friendly-snippets.enable = true;
+      sourcePlugins.codeium = {
+        enable = true;
+        package = $pkgs.vimPlugins.codeium-nvim;
+        module = "blink.compat.source";
+      };
+      setupOpts = {
+        keymap = {
+          preset = "none";
+          "<CR>" = [ "accept" "fallback" ];
+          "<Tab>" = [ "snippet_forward" "select_next" "fallback" ];
+          "<S-Tab>" = [ "snippet_backward" "select_prev" "fallback" ];
+        };
+        sources.default = [
+          "lsp"
+          "path"
+          "buffer"
+          "codeium"
+          "snippets"
+        ];
+      };
+    };
+    nvim-cmp = {
+      enable = true;
+      setupOpts.completion.completeopt = "";
+    };
+  };
+}
