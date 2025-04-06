db67e65be61cc37dcaa8f8d44901ff8039f1194f
Lukerator <lucadarius41@yahoo.com>
2025-04-06 20:55:23 +0300

    update

--- modules/nixvim/keymaps/misc/sortbylength.nix
+++ modules/nixvim/keymaps/misc/sortbylength.nix
@@ -4,6 +4,10 @@
       mode = "v";
       key = "<leader>ss";
       action = '': ! awk '{ print length(), $0 | "sort -n | cut -d\\  -f2-" }'<CR>'';
+      options = {
+        silent = true;
+        desc = "[S]ort line[s] by length";
+      };
     }
   ];
 }
