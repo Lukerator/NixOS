9118edb27a95785ce52f0770461b85f2541e773f
Lukerator <lucadarius41@yahoo.com>
2025-04-06 17:56:58 +0300

    update

--- modules/system/permanent/specialisations.nix
+++ modules/system/permanent/specialisations.nix
@@ -2,7 +2,7 @@
 {
   specialisation.gaming.configuration.hardware.nvidia.prime.reverseSync.enable = lib.mkForce true;
   specialisation.battery-saving.configuration.hardware = {
-    bluetooth.powerOnBoot = false;
+    bluetooth.powerOnBoot = lib.mkForce false;
     nvidia.prime.offload = {
       enable = lib.mkForce true;
       enableOffloadCmd = lib.mkForce true;
