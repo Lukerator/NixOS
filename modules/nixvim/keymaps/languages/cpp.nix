13055d9f9f78c529c70cdba41bf909d11692704f
Lukerator <lucadarius41@yahoo.com>
2025-04-07 18:47:40 +0300

    update

--- modules/nixvim/keymaps/languages/cpp.nix
+++ modules/nixvim/keymaps/languages/cpp.nix
@@ -1,10 +1,19 @@
 {
-  
-keymaps = [
+
+  keymaps = [
     {
       mode = "n";
       key = "<leader>gc";
       action = '':FloatermNew --autoclose=0 bash -c "g++ -o %:p:r %:p -g && clear && %:p:r && rm %:p:r"<CR>'';
+      options = {
+        silent = true;
+        desc = "[G]enerate, run and delete [C]++ file";
+      };
+    }
+    {
+      mode = "n";
+      key = "<leader>gx";
+      action = '':FloatermNew --autoclose=0 bash -c "g++ -o %:p:r %:p -g && clear && %:p:r"<CR>'';
       options = {
         silent = true;
         desc = "[G]enerate and run [C]++ file";
