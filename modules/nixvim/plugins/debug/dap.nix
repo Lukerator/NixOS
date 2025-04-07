e0fbd78dd49505e3ab2996bc89e3154934ae0c13
Lukerator <lucadarius41@yahoo.com>
2025-04-07 13:59:48 +0300

    update

--- modules/nixvim/plugins/debug/dap.nix
+++ modules/nixvim/plugins/debug/dap.nix
@@ -1,11 +1,20 @@
 { pkgs, ... }:
 {
   programs.nixvim.plugins = {
-    dap.enable = true;
     dap-ui.enable = true;
     dap-lldb = {
       enable = true;
       settings.codelldb_path = "${pkgs.vscode-extensions.vadimcn.vscode-lldb}/share/vscode/extensions/vadimcn.vscode-lldb/adapter/codelldb";
+    };
+    dap = {
+      enable = true;
+      signs = {
+        dapBreakpoint.text = "🛑";
+        dapBreakpointCondition.text = "🛑";
+        dapBreakpointRejected.text = "🚫";
+        dapLogPoint.text = "📝";
+        dapStopped.text = "🚦";
+      };
     };
   };
 }
