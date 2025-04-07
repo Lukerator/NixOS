f91d94ee8c1d8070a807c49025809d0ee8d765ef
Lukerator <lucadarius41@yahoo.com>
2025-04-07 19:26:37 +0300

    update

--- modules/nixvim/plugins/filetree/default.nix
+++ modules/nixvim/plugins/filetree/default.nix
@@ -4,6 +4,10 @@
     settings = {
       view_options.show_hidden = true;
       skip_confirm_for_simple_edits = true;
+      float = {
+        max_width = 0.65;
+        mak_heigth = 0.65;
+      };
     };
   };
 }
