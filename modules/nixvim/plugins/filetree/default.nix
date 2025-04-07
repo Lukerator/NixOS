55c54c8aec401f66de9d578b1ffbd2aa1b0a179c
Lukerator <lucadarius41@yahoo.com>
2025-04-07 19:24:19 +0300

    update

--- modules/nixvim/plugins/filetree/default.nix
+++ modules/nixvim/plugins/filetree/default.nix
@@ -1,6 +1,9 @@
 {
   plugins.oil = {
     enable = true;
-    settings.view_options.show_hidden = false;
+    settings = {
+      view_options.show_hidden = false;
+      skip_confirm_for_simple_edits = true;
+    };
   };
 }
