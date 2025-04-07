75b8983e7059c678e86adb8fd982aa71e8a99865
Lukerator <lucadarius41@yahoo.com>
2025-04-07 12:25:45 +0300

    update

---
+++ modules/nixvim/keymaps/plugins/debug/dap.nix
@@ -0,0 +1,21 @@
+{
+  programs.nixvim.keymaps = [
+    {
+      mode = "n";
+      key = "<leader>dc";
+      action.__raw = ''
+        function()
+          vim.fn.system("g++ " .. vim.fn.expand("%") .. " -o " .. vim.fn.expand("%:r"))
+          require("dap-ui").toggle()
+          require("dap").run({
+            type = "lldb",
+            name = "Debug",
+            request = "launch",
+            program = vim.fn.expand("%:r")
+          })
+          vim.fn.system("rm " .. vim.fn.expand("%:r"))
+        end
+      '';
+    }
+  ];
+}
