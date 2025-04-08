f859c384914f4d1fce0f8900126de993fd96698c
Lukerator <lucadarius41@yahoo.com>
2025-04-08 22:27:22 +0300

    update

--- modules/nixvim/keymaps/languages/haskell.nix
+++ modules/nixvim/keymaps/languages/haskell.nix
@@ -3,7 +3,7 @@
     {
       mode = "n";
       key = "<leader>gh";
-      action = '':FloatermNew --autoclose=0 ghc % && clear && %:r && rm %:r && rm %:r.o && rm %:r.hi<CR>'';
+      action = '':FloatermNew --autoclose=0 ghc %:p && clear && %:p:r && rm %:p:r && rm %:p:r.o && rm %:p:r.hi<CR>'';
       options = {
         silent = true;
         desc = "[G]enerate and run [H]askell file";
