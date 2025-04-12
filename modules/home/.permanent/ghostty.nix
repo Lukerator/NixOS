825bc6b6cfbde1a7b0e54973bc6f3e636b914911
Lukerator <lucadarius41@yahoo.com>
2025-04-12 18:07:44 +0300

    update

--- modules/home/.permanent/ghostty.nix
+++ modules/home/.permanent/ghostty.nix
@@ -4,6 +4,8 @@
     settings = {
       font-size = 12;
       theme = "tokyonight";
+      window-padding-x = 3;
+      window-padding-y = 3;
       font-family = "JetBrainsMonoNL Nerd Font Mono";
     };
   };
