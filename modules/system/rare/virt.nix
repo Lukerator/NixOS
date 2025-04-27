2f4a73adeedc89ca3607059ef09c4f8e21d71982
Lukerator <lucadarius41@yahoo.com>
2025-04-27 22:19:44 +0300

    update

--- modules/system/rare/virt.nix
+++ modules/system/rare/virt.nix
@@ -1,6 +1,8 @@
+{ pkgs, ... }:
 {
-  users.users.luke.extraGroups = [ "libvirtd" ];
   programs.virt-manager.enable = true;
+  users.users.luke.extraGroups = [ "libvirtd" ];
+  environment.systemPackages = [ pkgs.virtiofsd ];
   virtualisation = {
     libvirtd.enable = true;
     spiceUSBRedirection.enable = true;
