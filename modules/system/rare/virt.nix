ff1e5ca7091d99292e46399d97f8b4933a9c31e0
Lukerator <lucadarius41@yahoo.com>
2025-04-27 19:55:10 +0300

    update

--- modules/system/rare/virt.nix
+++ modules/system/rare/virt.nix
@@ -2,7 +2,7 @@
 {
   boot.kernelModules = [ "kvm-intel" ];
   users.users.luke.extraGroups = [ "vboxusers" ];
-  environment.systemPackages = [ pkgs.virtualboxKvm ];
+  environment.systemPackages = [ pkgs.gnome-boxes ];
   virtualisation.virtualbox.host = {
     enable = true;
     enableKvm = true;
